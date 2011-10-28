59055a163bd52322012c79b12031fe6a1fb3a506
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2011-10-28 23:56:56 +0300

    openrisc: interrupts.c whitespace and codingstyle cleanup

--- arch/openrisc/cpu/interrupts.c
+++ arch/openrisc/cpu/interrupts.c
@@ -1,4 +1,3 @@
-
 #include <asm/types.h>
 #include <asm/ptrace.h>
 #include <asm/system.h>
@@ -14,64 +13,59 @@ extern void _interrupt_handler(void);
 extern unsigned long _interrupt_handler_table;
 extern unsigned long _interrupt_handler_data_ptr_table;
 
-int
-interrupt_init(void)
+int interrupt_init(void)
 {
-
-	
 	/* install handler for external interrupt exception */
 	_exception_handler_add(8,_interrupt_handler);
-	
+
 	/* Enable interrupts in supervisor register */
-	mtspr (SPR_SR, mfspr (SPR_SR) | SPR_SR_IEE);
-	
+	mtspr(SPR_SR, mfspr(SPR_SR) | SPR_SR_IEE);
+
 	return 0;
 }
 
-void 
-enable_interrupts  (void)
+void enable_interrupts(void)
 {
 	/* Set interrupt enable bit in supervisor register */
-	mtspr (SPR_SR, mfspr (SPR_SR) | SPR_SR_IEE);
+	mtspr(SPR_SR, mfspr(SPR_SR) | SPR_SR_IEE);
 }
 
 
-int 
-disable_interrupts  (void)
+int disable_interrupts(void)
 {
 	/* Clear interrupt enable bit in supervisor register */
-	mtspr (SPR_SR, mfspr (SPR_SR) & ~SPR_SR_IEE);
-	/* 
-	 * HACK: Timer exception needs to be disabled before loading 
-	 * os since the exception vector will be trashed. 
+	mtspr(SPR_SR, mfspr(SPR_SR) & ~SPR_SR_IEE);
+	/*
+	 * HACK: Timer exception needs to be disabled before loading
+	 * os since the exception vector will be trashed.
 	 * TODO: Find a better hook in do_bootm than here.
 	 */
-	mtspr (SPR_SR, mfspr (SPR_SR) & ~SPR_SR_TEE);
+	mtspr (SPR_SR, mfspr(SPR_SR) & ~SPR_SR_TEE);
 	return 0;
 }
 
 #if defined(CONFIG_CMD_IRQ)
-int do_irqinfo (cmd_tbl_t * cmdtp, int flag, int argc, char * const argv[])
+int do_irqinfo(cmd_tbl_t * cmdtp, int flag, int argc, char * const argv[])
 {
 	int i;
-	ulong *handler, *arg; 
+	ulong *handler, *arg;
 
 	handler = &_interrupt_handler_table;
-	arg     = &_interrupt_handler_data_ptr_table;
+	arg = &_interrupt_handler_data_ptr_table;
 
-	printf ("\nInterrupt-Information:\n\n");
-	printf ("Nr  Routine   Arg\n");
-	printf ("-----------------\n");
+	printf("\nInterrupt-Information:\n\n");
+	printf("Nr  Routine   Arg\n");
+	printf("-----------------\n");
 
 	for (i=0; i<32; i++) {
 		if (handler[i] != INTERRUPT_HANDLER_NOT_SET) {
-			printf ("%02d  %08lx  %08lx\n",
+			printf("%02d  %08lx  %08lx\n",
 				i,
 				(ulong)handler[i],
 				(ulong)arg[i]);
 		}
 	}
-	printf ("\n");
+	printf("\n");
 
 	return (0);
 }
