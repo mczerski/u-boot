6d4dfff6ea76e4be2834da74a1cb19e3e1572d0d
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2011-10-28 06:34:03 +0300

    rtu-lite-511: print more specific board information
    Signed-off-by: Fredrik Bruhn <fredrik.bruhn@aacmicrotec.com>

--- board/rtu-lite-511/rtu-lite-511.c
+++ board/rtu-lite-511/rtu-lite-511.c
@@ -24,6 +24,52 @@
 
 #include <common.h>
 #include <netdev.h>
+#include <asm/gpio.h>
+
+// AAC AGC lookup table.
+
+#define RTU_lite_511 	0x31
+#define RTU_lite_512 	0x32
+#define uRTU_311 	0x21
+#define uRTU_312 	0x22
+
+//Get the AGC which is located on GPIO 44 to GPIO 51
+
+#define agc_bit0 	44
+#define agc_bit1 	45
+#define agc_bit2 	46
+#define agc_bit3 	47
+#define agc_bit4 	48
+#define agc_bit5 	49
+#define agc_bit6 	50
+#define agc_bit7 	51
+
+int get_aac_agc(void)
+{
+	ushort agc = 0x00;
+
+	// Define AGC IO
+	gpio_direction_input(agc_bit0);
+	gpio_direction_input(agc_bit1);
+	gpio_direction_input(agc_bit2);
+	gpio_direction_input(agc_bit3);
+	gpio_direction_input(agc_bit4);
+	gpio_direction_input(agc_bit5);
+	gpio_direction_input(agc_bit6);
+	gpio_direction_input(agc_bit7);
+
+	//Get the AGC which is located on GPIO 44 to GPIO 51
+	agc = agc + gpio_get_value(agc_bit0) * 1;
+	agc = agc + gpio_get_value(agc_bit1) * 2;
+	agc = agc + gpio_get_value(agc_bit2) * 4;
+	agc = agc + gpio_get_value(agc_bit3) * 8;
+	agc = agc + gpio_get_value(agc_bit4) * 16;
+	agc = agc + gpio_get_value(agc_bit5) * 32;
+	agc = agc + gpio_get_value(agc_bit6) * 64;
+	agc = agc + gpio_get_value(agc_bit7) * 128;
+
+	return agc;
+}
 
 int board_early_init_f(void)
 {
@@ -32,7 +78,28 @@ int board_early_init_f(void)
 
 int checkboard(void)
 {
-	printf("BOARD : %s\n", CONFIG_BOARD_NAME);
+	ushort agc = get_aac_agc();
+
+	if ( agc == RTU_lite_511 )
+	{
+  		printf("BOARD: AAC Microtec RTU lite 511 (EM)\n");
+	}
+
+	if ( agc == RTU_lite_512 )
+	{
+  		printf("BOARD: AAC Microtec RTU lite 512 (FM)\n");
+	}
+
+	if ( agc == uRTU_311 )
+	{
+  		printf("BOARD: AAC Microtec uRTU 311 (EM)\n");
+	}
+
+	if ( agc == uRTU_312 )
+	{
+  		printf("BOARD: AAC Microtec uRTU 312 (FM)\n");
+	}
+
 	return 0;
 }
 
