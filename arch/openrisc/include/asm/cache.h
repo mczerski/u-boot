f6fadcd2fdfbd8556235126b50c76c86201fa0d4
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2011-10-29 08:19:00 +0300

    openrisc: remove flush_{d|i}cache from cache.h
    declaring them here clashes with cmd_cache

--- arch/openrisc/include/asm/cache.h
+++ arch/openrisc/include/asm/cache.h
@@ -1,7 +1,4 @@
 #ifndef __ASM_OPENRISC_CACHE_H_
 #define __ASM_OPENRISC_CACHE_H_
 
-extern void flush_dcache (unsigned long start, unsigned long size);
-extern void flush_icache (unsigned long start, unsigned long size);
-
 #endif /* __ASM_OPENRISC_CACHE_H_ */
