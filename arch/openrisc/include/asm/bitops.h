a254626fb9f189e23cd8346c9cabadd83cc9bfcb
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2011-11-03 22:33:39 +0200

    openrisc: add optimized fls and ffs bitops

--- arch/openrisc/include/asm/bitops.h
+++ arch/openrisc/include/asm/bitops.h
@@ -1,5 +1,9 @@
 #ifndef __ASM_OPENRISC_BITOPS_H
 #define __ASM_OPENRISC_BITOPS_H
 
+#define PLATFORM_FLS
+#include <asm/bitops/fls.h>
+#define PLATFORM_FFS
+#include <asm/bitops/ffs.h>
 
 #endif /* __ASM_GENERIC_BITOPS_H */
