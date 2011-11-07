fa00ab99c3f1b9dc72d56118961c4872b2255647
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2011-11-07 05:50:16 +0200

    de2_115: config cleanup

--- include/configs/de2_115.h
+++ include/configs/de2_115.h
@@ -24,48 +24,44 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
-/* enable this for debug information */
-/* #define DEBUG*/
-
 /*
  * BOARD/CPU
  */
 
-#define CONFIG_SYS_CLK_FREQ 50000000
-#define CONFIG_SYS_RESET_ADDR 0x00000100
+#define CONFIG_SYS_CLK_FREQ		50000000
+#define CONFIG_SYS_RESET_ADDR		0x00000100
 
-#define CONFIG_SYS_SDRAM_BASE 0x00000000
-#define CONFIG_SYS_SDRAM_SIZE 0x02000000
+#define CONFIG_SYS_SDRAM_BASE		0x00000000
+#define CONFIG_SYS_SDRAM_SIZE		0x02000000
 
-#define CONFIG_SYS_UART_BASE 0x90000000
-#define CONFIG_SYS_UART_FREQ CONFIG_SYS_CLK_FREQ
-#define CONFIG_SYS_UART_BAUD 115200
+#define CONFIG_SYS_UART_BASE		0x90000000
+#define CONFIG_SYS_UART_FREQ		CONFIG_SYS_CLK_FREQ
+#define CONFIG_SYS_UART_BAUD		115200
 
-#define CONFIG_BOARD_NAME "de2_115" /* custom board name */
+#define CONFIG_BOARD_NAME		"de2_115" /* custom board name */
 
 #define CONFIG_SYS_NO_FLASH
-#define CONFIG_SYS_MAX_FLASH_SECT 0
+#define CONFIG_SYS_MAX_FLASH_SECT	0
 
 /*
  * SERIAL
  */
-# define CONFIG_SYS_NS16550		1
+# define CONFIG_SYS_NS16550
 # define CONFIG_SYS_NS16550_SERIAL
 # define CONFIG_SYS_NS16550_REG_SIZE	1
 # define CONFIG_CONS_INDEX		1
-# define CONFIG_SYS_NS16550_COM1 \
-			(0x90000000)
-# define CONFIG_SYS_NS16550_CLK	CONFIG_SYS_CLK_FREQ
+# define CONFIG_SYS_NS16550_COM1	(0x90000000)
+# define CONFIG_SYS_NS16550_CLK		CONFIG_SYS_CLK_FREQ
 
-#define CONFIG_BAUDRATE		CONFIG_SYS_UART_BAUD
+#define CONFIG_BAUDRATE			CONFIG_SYS_UART_BAUD
 #define CONFIG_SYS_BAUDRATE_TABLE	{CONFIG_BAUDRATE}
 #define CONFIG_SYS_CONSOLE_INFO_QUIET	/* Suppress console info */
-#define CONSOLE_ARG	"console=console=ttyS0,115200\0"
+#define CONSOLE_ARG			"console=console=ttyS0,115200\0"
 
 /*
  * TIMER
  */
-#define CONFIG_SYS_HZ			100 
+#define CONFIG_SYS_HZ			1000
 
 /*
  * MMC over SPI
@@ -84,30 +80,45 @@
 #define CONFIG_CMD_MMC_SPI
 #define CONFIG_CMD_FAT
 
+/*
+ * Memory organisation:
+ *
+ * RAM start ---------------------------
+ *           | ...                     |
+ *           ---------------------------
+ *           | Stack                   |
+ *           ---------------------------
+ *           | Global data             |
+ *           ---------------------------
+ *           | Environment             |
+ *           ---------------------------
+ *           | Monitor                 |
+ * RAM end   ---------------------------
+ */
 /* We're running in RAM */
 #define CONFIG_MONITOR_IS_IN_RAM
 #define CONFIG_SYS_MONITOR_LEN	0x80000	/* Reserve 512k */
 #define CONFIG_SYS_MONITOR_BASE	(CONFIG_SYS_SDRAM_BASE + \
-				 CONFIG_SYS_SDRAM_SIZE - \
-				 CONFIG_SYS_MONITOR_LEN)
+				CONFIG_SYS_SDRAM_SIZE - \
+				CONFIG_SYS_MONITOR_LEN)
 
-#define CONFIG_ENV_IS_NOWHERE	1
+#define CONFIG_ENV_IS_NOWHERE
 #define CONFIG_ENV_SIZE		0x20000 /* Total Size of Environment, 128KB */
-#define CONFIG_ENV_ADDR         (CONFIG_SYS_MONITOR_BASE - CONFIG_ENV_SIZE)
+#define CONFIG_ENV_ADDR		(CONFIG_SYS_MONITOR_BASE - CONFIG_ENV_SIZE)
 
 /*
  * Global data object and stack pointer
  */
 #define CONFIG_SYS_GBL_DATA_OFFSET	(CONFIG_ENV_ADDR \
 					- GENERATED_GBL_DATA_SIZE)
-#define CONFIG_SYS_GBL_DATA_ADDR     CONFIG_SYS_GBL_DATA_OFFSET
-#define CONFIG_SYS_INIT_SP_ADDR      CONFIG_SYS_GBL_DATA_OFFSET
-#define CONFIG_SYS_INIT_SP_OFFSET      CONFIG_SYS_GBL_DATA_OFFSET
-#define CONFIG_SYS_STACK_LENGTH        0x10000 /* 64KB */
-#define CONFIG_SYS_MALLOC_LEN          0x400000 /* 4MB */
-#define CONFIG_SYS_MALLOC_BASE         (CONFIG_SYS_INIT_SP_OFFSET \
-	                                - CONFIG_SYS_STACK_LENGTH \
-	                                - CONFIG_SYS_MALLOC_LEN)
+#define CONFIG_SYS_GBL_DATA_ADDR	CONFIG_SYS_GBL_DATA_OFFSET
+#define CONFIG_SYS_INIT_SP_ADDR		CONFIG_SYS_GBL_DATA_OFFSET
+#define CONFIG_SYS_INIT_SP_OFFSET	CONFIG_SYS_GBL_DATA_OFFSET
+#define CONFIG_SYS_STACK_LENGTH		0x10000 /* 64KB */
+#define CONFIG_SYS_MALLOC_LEN		0x400000 /* 4MB */
+#define CONFIG_SYS_MALLOC_BASE		(CONFIG_SYS_INIT_SP_OFFSET \
+					- CONFIG_SYS_STACK_LENGTH \
+					- CONFIG_SYS_MALLOC_LEN)
 /*
  * MISC
  */
@@ -118,12 +129,11 @@
 #define CONFIG_SYS_BARGSIZE		CONFIG_SYS_CBSIZE /* Bootarg buf size */
 #define CONFIG_SYS_PBSIZE		(CONFIG_SYS_CBSIZE + \
 					sizeof(CONFIG_SYS_PROMPT) + \
-					 16)	/* Print buf size */
+					16)	/* Print buf size */
 #define CONFIG_SYS_LOAD_ADDR		CONFIG_SYS_SDRAM_BASE
 #define CONFIG_SYS_MEMTEST_START	(CONFIG_SYS_SDRAM_BASE + 0x2000)
 #define CONFIG_SYS_MEMTEST_END		(CONFIG_SYS_INIT_SP_ADDR - 0x20000)
 #define CONFIG_CMDLINE_EDITING
-
 
 /*
  * Command line configuration.
@@ -135,14 +145,9 @@
 
 #define CONFIG_CMD_IRQ
 #define CONFIG_CMD_ELF
-/*
-#define CONFIG_CMD_DATE
-#define CONFIG_CMD_I2C
-*/
 #define CONFIG_CMD_BSP
 
 #define CONFIG_OF_LIBFDT
 #define CONFIG_LMB
-
 
 #endif /* __CONFIG_H */
