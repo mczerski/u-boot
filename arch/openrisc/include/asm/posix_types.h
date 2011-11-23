13ec2b529c7e4138d495657c3ac54186b30cd761
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2011-11-23 06:05:39 +0200

    openrisc: add copyright/licensing info to asm/ header files

--- arch/openrisc/include/asm/posix_types.h
+++ arch/openrisc/include/asm/posix_types.h
@@ -1,3 +1,17 @@
+/*
+ * Based on microblaze implementation:
+ *  Copyright (C) 2003       John Williams <jwilliams@itee.uq.edu.au>
+ *  Copyright (C) 2001,2002  NEC Corporation
+ *  Copyright (C) 2001,2002  Miles Bader <miles@gnu.org>
+ *
+ * This file is subject to the terms and conditions of the GNU General
+ * Public License.  See the file COPYING in the main directory of this
+ * archive for more details.
+ *
+ * Written by Miles Bader <miles@gnu.org>
+ * Microblaze port by John Williams
+ */
+
 #ifndef __ASM_OPENRISC_POSIX_TYPES_H
 #define __ASM_OPENRISC_POSIX_TYPES_H
 
