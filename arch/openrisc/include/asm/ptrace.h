126beef92ccc1ae0d17aace7ec995d1295872159
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2011-11-23 06:04:45 +0200

    openrisc: bring in up to date ptrace.h from Linux

--- arch/openrisc/include/asm/ptrace.h
+++ arch/openrisc/include/asm/ptrace.h
@@ -1,83 +1,131 @@
+/*
+ * OpenRISC Linux
+ *
+ * Linux architectural port borrowing liberally from similar works of
+ * others.  All original copyrights apply as per the original source
+ * declaration.
+ *
+ * OpenRISC implementation:
+ * Copyright (C) 2003 Matjaz Breskvar <phoenix@bsemi.com>
+ * Copyright (C) 2010-2011 Jonas Bonn <jonas@southpole.se>
+ * et al.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
 #ifndef __ASM_OPENRISC_PTRACE_H
 #define __ASM_OPENRISC_PTRACE_H
 
 #include <asm/spr-defs.h>
+
+#ifndef __ASSEMBLY__
 /*
- * This struct defines the way the registers are stored on the
- * kernel stack during a system call or other kernel entry.
- *
- * this should only contain volatile regs
- * since we can keep non-volatile in the thread_struct
- * should set this up when only volatiles are saved
- * by intr code.
- *
- * Since this is going on the stack, *CARE MUST BE TAKEN* to insure
- * that the overall structure is a multiple of 16 bytes in length.
- *
- * Note that the offsets of the fields in this struct correspond with
- * the values below.
+ * This is the layout of the regset returned by the GETREGSET ptrace call
+ */
+struct user_regs_struct {
+	/* GPR R0-R31... */
+	unsigned long gpr[32];
+	unsigned long pc;
+	unsigned long sr;
+	unsigned long pad1;
+	unsigned long pad2;
+};
+#endif
+
+#ifdef __KERNEL__
+
+/*
+ * Make kernel PTrace/register structures opaque to userspace... userspace can
+ * access thread state via the regset mechanism.  This allows us a bit of
+ * flexibility in how we order the registers on the stack, permitting some
+ * optimizations like packing call-clobbered registers together so that
+ * they share a cacheline (not done yet, though... future optimization).
  */
 
 #ifndef __ASSEMBLY__
+/*
+ * This struct describes how the registers are laid out on the kernel stack
+ * during a syscall or other kernel entry.
+ *
+ * This structure should always be cacheline aligned on the stack.
+ * FIXME: I don't think that's the case right now.  The alignment is
+ * taken care of elsewhere... head.S, process.c, etc.
+ */
 
 struct pt_regs {
+	union {
+		struct {
+			/* Named registers */
+			long  sr;	/* Stored in place of r0 */
+			long  sp;	/* r1 */
+		};
+		struct {
+			/* Old style */
+			long offset[2];
+			long gprs[30];
+		};
+		struct {
+			/* New style */
+			long gpr[32];
+		};
+	};
 	long  pc;
-	long  sr;
-	long  sp;
-	long  gprs[30];
-	long  orig_gpr11;  /* Used for restarting system calls */
-	long  result;     /* Result of a system call */
-	long  syscallno;  /* Syscall no. (used by strace) */
+	long  orig_gpr11;	/* For restarting system calls */
+	long  syscallno;	/* Syscall number (used by strace) */
+	long dummy;		/* Cheap alignment fix */
 };
 #endif /* __ASSEMBLY__ */
 
-#ifdef __KERNEL__
+/* TODO: Rename this to REDZONE because that's what it is */
 #define STACK_FRAME_OVERHEAD  128  /* size of minimum stack frame */
 
-#define instruction_pointer(regs) ((regs)->pc)
-#define user_mode(regs) (((regs)->sr & SPR_SR_SM) == 0)
-#define profile_pc(regs) instruction_pointer(regs)
-
-#endif /* __KERNEL__ */
+#define instruction_pointer(regs)	((regs)->pc)
+#define user_mode(regs)			(((regs)->sr & SPR_SR_SM) == 0)
+#define user_stack_pointer(regs)	((unsigned long)(regs)->sp)
+#define profile_pc(regs)		instruction_pointer(regs)
 
 /*
  * Offsets used by 'ptrace' system call interface.
  */
-#define PC        0
-#define SR        4
-#define SP        8
-#define GPR2      12
-#define GPR3      16
-#define GPR4      20
-#define GPR5      24
-#define GPR6      28
-#define GPR7      32
-#define GPR8      36
-#define GPR9      40
-#define GPR10     44
-#define GPR11     48
-#define GPR12     52
-#define GPR13     56
-#define GPR14     60
-#define GPR15     64
-#define GPR16     68
-#define GPR17     72
-#define GPR18     76
-#define GPR19     80
-#define GPR20     84
-#define GPR21     88
-#define GPR22     92
-#define GPR23     96
-#define GPR24     100
-#define GPR25     104
-#define GPR26     108
-#define GPR27     112
-#define GPR28     116
-#define GPR29     120
-#define GPR30     124
-#define GPR31     128
-#define ORIG_GPR11 132
-#define RESULT    136
-#define SYSCALLNO 140
+#define PT_SR         0
+#define PT_SP         4
+#define PT_GPR2       8
+#define PT_GPR3       12
+#define PT_GPR4       16
+#define PT_GPR5       20
+#define PT_GPR6       24
+#define PT_GPR7       28
+#define PT_GPR8       32
+#define PT_GPR9       36
+#define PT_GPR10      40
+#define PT_GPR11      44
+#define PT_GPR12      48
+#define PT_GPR13      52
+#define PT_GPR14      56
+#define PT_GPR15      60
+#define PT_GPR16      64
+#define PT_GPR17      68
+#define PT_GPR18      72
+#define PT_GPR19      76
+#define PT_GPR20      80
+#define PT_GPR21      84
+#define PT_GPR22      88
+#define PT_GPR23      92
+#define PT_GPR24      96
+#define PT_GPR25      100
+#define PT_GPR26      104
+#define PT_GPR27      108
+#define PT_GPR28      112
+#define PT_GPR29      116
+#define PT_GPR30      120
+#define PT_GPR31      124
+#define PT_PC	      128
+#define PT_ORIG_GPR11 132
+#define PT_SYSCALLNO  136
+
+#endif /* __KERNEL__ */
 
 #endif /* __ASM_OPENRISC_PTRACE_H */
